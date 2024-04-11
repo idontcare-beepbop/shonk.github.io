de15abf89b98d098e77d00920a8b200024515e61
sshonkk <czarnobialy.nosek@gmail.com>
2024-04-11 14:38:52 +0200

    Update repository

--- update.bat
+++ update.bat
@@ -2,12 +2,18 @@
 
 REM Add all changes
 echo "Adding -all changes"
+echo "Adding -all changes"
+echo "Adding -all changes"
 git add --all
 
 REM Commit changes with a default 
 echo "Commining -m and Updating the repository"
+echo "Commining -m and Updating the repository"
+echo "Commining -m and Updating the repository"
 git commit -m "Update repository"
 
 REM Push changes to the main branch
+echo "Pushing the -u origin into main"
+echo "Pushing the -u origin into main"
 echo "Pushing the -u origin into main"
 git push -u origin main
